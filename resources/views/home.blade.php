cf9f4e3429ee7f8c533260e2ab0269c32014e88e
Yveskk <yves.kokou10@gmail.com>
2022-05-24 23:35:57 +0000

    traduction

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,14 @@
                 <div class="widget-body">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="state">
-                            <h6>Vechile In Today</h6>
+                            <h6>Vehicule aujourd’hui</h6>
                             <h2>{{ $total_vehicle_in }}</h2>
                         </div>
                         <div class="icon">
                             <i class="ik ik-truck"></i>
                         </div>
                     </div>
-                    <small class="text-small mt-10 d-block">6% higher than last month</small>
+                    <small class="text-small mt-10 d-block">6% de plus que le mois dernier</small>
                 </div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="62" aria-valuemin="0" aria-valuemax="100" style="width: 62%;"></div>
@@ -27,14 +27,14 @@
                 <div class="widget-body">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="state">
-                            <h6>Vechile Out Today</h6>
+                            <h6>Vehicule hors aujourd’hui</h6>
                             <h2>{{ $total_vehicle_out }}</h2>
                         </div>
                         <div class="icon">
                             <i class="ik ik-truck"></i>
                         </div>
                     </div>
-                    <small class="text-small mt-10 d-block">61% higher than last month</small>
+                    <small class="text-small mt-10 d-block">61% de plus que le mois dernier</small>
                 </div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-success" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100" style="width: 78%;"></div>
@@ -46,14 +46,14 @@
                 <div class="widget-body">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="state">
-                            <h6>Total Vechiles</h6>
+                            <h6>Vehicule Total</h6>
                             <h2>{{ $total_vehicles }}</h2>
                         </div>
                         <div class="icon">
                             <i class="ik ik-truck"></i>
                         </div>
                     </div>
-                    <small class="text-small mt-10 d-block">Total Vechiles</small>
+                    <small class="text-small mt-10 d-block">Vehicule Total</small>
                 </div>
                 <div class="progress progress-sm">
                     <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="31" aria-valuemin="0" aria-valuemax="100" style="width: 31%;"></div>
